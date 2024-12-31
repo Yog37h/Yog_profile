f2cfa6d6e7dcfb74f336c9062a6f60c80435645b
Yogesh <118930142+Yog37h@users.noreply.github.com>
2024-12-31 17:29:02 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -41,5 +41,5 @@ disableLogger: true,
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+//automaticVercelMonitors: true,
 });
