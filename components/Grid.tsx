53a7ce094e7669a5813ac773f1c3a5262138bd8f
Yog37h <kiyogesh80@gmail.com>
2024-12-31 17:21:33 +0530

    Initial commit

---
+++ components/Grid.tsx
@@ -0,0 +1,31 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const Grid = () => {
+  if (typeof window === "undefined") {
+    // Prevent SSR from rendering the component improperly
+    return null;
+  }
+
+  return (
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
